19f2d121e08437cfd90e4f769bc5cb1c3a09d1bd
Ido Cohen <ido.c.mail@gmail.com>
2016-01-30 18:52:01 +0200

    Adding a refresh button

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by idoco on 29/1/2016.
+ * buttons logic
+ */
+
+function refreshMap() {
+    var iFrame = document.getElementById("mapFrame");
+    //noinspection SillyAssignmentJS
+    iFrame.src = iFrame.src;
+}
